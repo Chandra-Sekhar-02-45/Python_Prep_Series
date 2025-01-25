2ddc432ba32fd95bdda56bd9a2e921d6949a1f97
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-25 17:16:43 +0530

    tetNumber is added

---
+++ Problems_Python/testNumber.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number: "))
+
+"Checking the number is Even Number or Odd Number"
+if number % 2 == 0:
+    print(str(number) + " is an Even Number")
+else:
+    print(str(number) + " is an Odd Number")
+
+"Checking the number is Prime Number"
