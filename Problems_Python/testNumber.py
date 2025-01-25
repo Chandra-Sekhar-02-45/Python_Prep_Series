cff8553bfb985d4c18509a8f25b34687a4ddbd03
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-25 17:16:56 +0530

    tetNumber is added

--- Problems_Python/testNumber.py
+++ Problems_Python/testNumber.py
@@ -1,5 +1,14 @@
 number = int(input("Enter a number: "))
 
+
+if number > 0:
+    print(str(number)+ " is a Natural Number")
+elif number == 0:
+    print(str(number) + " is a Whole Number")
+    print(str(number)+ " is a Non Negative Number")
+else:
+    print(str(number)+ " is a Negative Number")
+
 "Checking the number is Even Number or Odd Number"
 if number % 2 == 0:
     print(str(number) + " is an Even Number")
@@ -7,3 +16,5 @@ else:
     print(str(number) + " is an Odd Number")
 
 "Checking the number is Prime Number"
+if number == 2:
+    print(str(number) + " is an EvenPrime Number")
