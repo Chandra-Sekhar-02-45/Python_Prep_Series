d014d87c81ccdb3162959906b549ab3ba5a061e9
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-04 20:53:45 +0530

    code completed

--- Problems_Python/nthDayOfThedFirstDay.py
+++ Problems_Python/nthDayOfThedFirstDay.py
@@ -1,7 +1,12 @@
-# This program determines the day of the week after a given number of days from a specified starting day. It assigns numerical values to days, applies modular arithmetic to handle the week cycle, and outputs the corresponding future day
+# This program determines the day of the week after a given number of days from a specified starting day.
+# It assigns numerical values to days, applies modular arithmetic to handle the week cycle, and outputs the corresponding future day
 
-d = input("Enter the day of the Week, Where you want to start the Month")
-n = int(input("Enter the number of days to find the corresponding day of the week:")) % 7  
+print("\nThe input day should be in the format: Monday, Tuesday, etc., and 'n' should be between 1 and 31.")
+
+d = input("Enter the day of the Week, Where you want to start the Month: ")
+n = int(input("Enter the number of days to find the corresponding day of the week: "))
+
+count = n % 7
 
 if d == "Monday":
     day = 1
@@ -15,22 +20,22 @@ elif d == "Friday":
     day = 5
 elif d == "Saturday":
     day = 6
-else:
+elif d == "Sunday":
     day = 0  
 
-week = (day + n - 1) % 7  
+week = (day + count - 1) % 7
 
 if week == 1:
-    print("Monday")
+    print(f"If the month starts with {d}, then the day after {n} days will be " + "Monday.")
 elif week == 2:
-    print("Tuesday")
+    print(f"If the month starts with {d}, then the day after {n} days will be " + "Tuesday.")
 elif week == 3:
-    print("Wednesday")
+    print(f"If the month starts with {d}, then the day after {n} days will be " + "Wednesday")
 elif week == 4:
-    print("Thursday")
+    print(f"If the month starts with {d}, then the day after {n} days will be " + "Thursday")
 elif week == 5:
-    print("Friday")
+    print(f"If the month starts with {d}, then the day after {n} days will be " + "Friday")
 elif week == 6:
-    print("Saturday")
-else:
-    print("Sunday")
+    print(f"If the month starts with {d}, then the day after {n} days will be " + "Saturday")
+elif week == 0:
+    print(f"If the month starts with {d}, then the day after {n} days will be " + "Sunday")
