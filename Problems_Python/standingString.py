936afc574d6ac66ed9bcd5b95590c73a19b3b8e1
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-09 13:07:10 +0530

    standing String code is added

---
+++ Problems_Python/standingString.py
@@ -0,0 +1,11 @@
+# This code is used to rewrite the strings in vertical order
+
+word = input("\nEnter a word: ")
+
+word_length = len(word)
+count = 0
+
+while count < word_length:
+     print(word[count])
+     count += 1
+
