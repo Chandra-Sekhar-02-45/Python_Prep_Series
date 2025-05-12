ad957cee19c5150b435f5eb3dba928c770105bcf
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-12 18:14:48 +0530

    prime numbers upp to number n

--- Problems_Python/primenumbers.py
+++ Problems_Python/primenumbers.py
@@ -1,9 +1,9 @@
-number = int(input())
+number = int(input("Enter a number: "))
 
-for i in range(2 , number + 1) :
+for i in range(2 , number + 1):
     factors = 0
-    for j in range(2 , i ) :
-        if number % j == 0 :
+    for j in range(2 , i):
+        if i % j == 0:
             factors += 1
-    if factors == 0 :
+    if factors == 0:
         print(i)
