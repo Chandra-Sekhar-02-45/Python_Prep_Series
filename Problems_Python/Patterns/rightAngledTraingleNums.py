643d158abc60d3ecece5fe7e34959b3b789c3819
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-13 11:20:10 +0530

    right angled triangle nums

--- Problems_Python/Patterns/rightAngledTraingleNums.py
+++ Problems_Python/Patterns/rightAngledTraingleNums.py
@@ -2,5 +2,15 @@ n = int(input())
 
 for i in range(n) :
     for j in range(i + 1) :
-        print( j + 1, end = " ")
+        print(j + 1 , end = " ")
     print()
+
+
+
+
+# n = int(input())
+# row = ""
+#
+# for i in range(1 , n + 1 ) :
+#     row += str(i) + " "
+#     print(row)
