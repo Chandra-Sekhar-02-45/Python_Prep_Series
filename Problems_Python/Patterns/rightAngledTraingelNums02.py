de8445d288685147db5ea14c67bbb47a951331dd
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-13 11:46:25 +0530

    rightangledtriangel2

---
+++ Problems_Python/Patterns/rightAngledTraingelNums02.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(n + 1) :
+    for j in range(i + 1):
+        print(i + 1 , end = " ")
+    print()
+
+#Single Loop
+# n = int(input())
+#
+# for i in range(1 , n + 1):
+#     print((str(i)  + " ") * i)
+#
