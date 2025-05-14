bdd5471b9a5e5fcd660590805de6e6d70fa0bf2a
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-14 12:41:49 +0530

    pyramd is done

--- Problems_Python/Patterns/pyramid.py
+++ Problems_Python/Patterns/pyramid.py
@@ -6,3 +6,9 @@ for i in range(n):
     for j in range(i + 1):
         print("*", end=" ")
     print()
+
+# Single Loop
+# n = int(input("Enter the number of rows for the star pyramid: "))
+
+# for i in range(n):
+#     print(" " * (n - i - 1) + "* " * (i + 1))
