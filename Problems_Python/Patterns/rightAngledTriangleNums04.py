b4b82b590eb9d32f541d9acabaf427349d514874
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-05-14 15:05:48 +0530

    rigt angle triangle 04

---
+++ Problems_Python/Patterns/rightAngledTriangleNums04.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n) :
+    print(" " * 2*(n - i - 1) , end = "")
+    k = i + 1
+    for j in range(i + 1) :
+        print(k , end = " ")
+        k -= 1
+    print()
