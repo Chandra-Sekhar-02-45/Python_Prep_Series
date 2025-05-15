3443192318b2aa2217ca59906922fdf376bd6a12
R.V.Chandra Sekhara Raju <rvcsraju@gmail.com>
2025-05-15 14:16:09 +0530

    Update armstrongUpToN.py

--- Problems_Python/armstrongUpToN.py
+++ Problems_Python/armstrongUpToN.py
@@ -2,25 +2,16 @@
 # An Armstrong number is a number that is equal to the sum of its digits, each raised to the power of the number of digits in the number.
 
 # Taught On Approach:
-            # Input the upper limit n:
-            # Start a loop from 1 to n:
-            # Convert number to string:
-            # Count the length of numbers (k):
-            # Initialize a sum variable (temp_sum):
-            # Loop through each digit in the number:
-            # Raise the digit to the power of k:
-            # Add the result to the sum variable:
-            # If the sum is equal to the original number, it’s an Armstrong number.
 
+n = int(input("Enter a number: "))     # Input the upper limit n:
 
-n = int(input("Enter a number: "))
-
-for i in range(1, n + 1):
-    a = str(i)
-    k = len(a)
-    temp_sum = 0
-    for j in a:
-        temp_sum += int(j) ** k
-    if temp_sum == i:
+for i in range(1, n + 1):              # Start a loop from 1 to n:
+    a = str(i)                         # Convert number to string:
+    k = len(a)                         # Count the length of numbers (k):
+    temp_sum = 0                       # Initialize a sum variable (temp_sum):
+    for j in a:                        # Loop through each digit in the string a, which represents the number.
+        temp_sum += int(j) ** k        # Raise the digit to the power of k & Add the result to the sum variable:
+                
+    if temp_sum == i:                  # If the sum is equal to the original number, it’s an Armstrong number.
         print(i)
 
