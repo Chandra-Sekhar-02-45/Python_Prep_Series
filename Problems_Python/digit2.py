03084138070612a16232f22c90ab5f0982acabb3
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-06-05 23:40:20 +0530

    digit2

---
+++ Problems_Python/digit2.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if j == n or i == 1 or i == n:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+for i in range(1, n):
+    for j in range(1, n + 1):
+        if i == n - 1 or j == 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+
