81d373eb00244d524a801fb8af658475e9fad31b
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-06-05 13:36:48 +0530

    digit 6

---
+++ Problems_Python/Patterns/digit6.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for i in range(1, n):
+    if i == 1:
+        star = "* " * n
+        print(star)
+    else:
+        for j in range(1, n):
+            if j == 1:
+                print("* ", end=" ")
+        print()
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == 1 or i == n or j == 1 or j == n:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
